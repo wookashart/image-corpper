a931f26e7490f1e33a893b9b147a0ff25f3865e1
Lukasz Krawczyk <lukasz.krawczyk@vmlyr.com>
2024-08-24 21:21:05 +0200

    feat: initial configuration

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# image-corpper
+## Getting Started
+
+Install yarn ([https://yarnpkg.com/](https://yarnpkg.com/)):
+
+```bash
+yarn
+```
+
+## Next.js
+
+[https://nextjs.org/](https://nextjs.org/)
+
+Run the development server:
+
+```bash
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ app/error.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    // eslint-disable-next-line no-console
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>Error: {error.message}</div>
+  );
+}
